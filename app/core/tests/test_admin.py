c8b24d7b8549ca4152d77af91a3c5b6ce32c72a7
Tom <abaotom14@gmail.com>
2020-10-29 08:16:17 +0800

    add AdminSiteTests

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,19 @@
+from django.test import Client, TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='password'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@example.com',
+            password='password',
+            name='Test'
+        )
