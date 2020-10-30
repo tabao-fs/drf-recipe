3e6ae4479e6ea81658acbb3d77e0106a5a7722db
Tom <abaotom14@gmail.com>
2020-10-30 09:32:05 +0800

    add apps

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'core'
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    'core',
+    'users'
 ]
 
 MIDDLEWARE = [
