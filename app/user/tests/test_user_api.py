f5db521fff827ea5b0b9da60fd330e3f7449a467
Tom <abaotom14@gmail.com>
2020-10-31 08:45:24 +0800

    add PublicUserApiTests class

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    '''
+    Test the users API (public)
+    '''
+    def setUp(self):
+        self.client = APIClient()
