a1653bc0c40d29c6ff39beedf8c5406fe3c5e385
Tom <abaotom14@gmail.com>
2020-11-03 08:38:44 +0800

    add test_update_user_profile

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -167,3 +167,16 @@ class PrivateUserApiTests(TestCase):
         res = self.client.post(ME_URL, {})
 
         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        '''
+        Test updating the user profile for authenticated user
+        '''
+        payload = {'name': 'Test', 'password': 'password'}
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
