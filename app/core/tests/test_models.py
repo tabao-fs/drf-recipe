b9d466a2a642065ddc87242180056527af0ced0d
Tom <abaotom14@gmail.com>
2020-11-04 08:45:05 +0800

    add test_tag_str

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
 
 def sample_user(email='test@example.com', password='password'):
     '''
@@ -53,3 +55,14 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        '''
+        Test the tag string representation
+        '''
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Tag'
+        )
+
+        self.assertEqual(str(tag), tag.name)
