5e0fc8f9d35c9a411d6a5f1958776d3f8b37e2bb
Tom <abaotom14@gmail.com>
2020-11-04 08:54:51 +0800

    add PublicTagsApiTests

---
+++ app/recipe/tests/test_tags_api.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag
+
+
+class PublicTagsApiTests(TestCase):
+    '''
+    Test the publicly available tags API
+    '''
+    def setUp(self):
+        self.client = APIClient()
