0c01df3bb37109452e1e04700a6cecaeb5ddef63
Tom <abaotom14@gmail.com>
2020-11-04 08:44:20 +0800

    add sample_user

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,6 +2,13 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
+def sample_user(email='test@example.com', password='password'):
+    '''
+    Create a sample user
+    '''
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
