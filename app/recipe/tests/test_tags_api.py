b2b6479bc9b4e4113bd1979416a7ba3e35c82b7a
Tom <abaotom14@gmail.com>
2020-11-04 08:56:36 +0800

    add test_login_required

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -8,9 +8,20 @@ from rest_framework.test import APIClient
 from core.models import Tag
 
 
+TAGS_URL = reverse('recipe:tag-list')
+
+
 class PublicTagsApiTests(TestCase):
     '''
     Test the publicly available tags API
     '''
     def setUp(self):
         self.client = APIClient()
+
+    def test_login_required(self):
+        '''
+        Test that login is required for retrieving tags
+        '''
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
