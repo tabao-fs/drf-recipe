075c601db757cbd594667195740284b7033ee853
Tom <abaotom14@gmail.com>
2020-11-05 08:49:10 +0800

    use BaseRecipeAttrViewSet

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -29,50 +29,17 @@ class BaseRecipeAttrViewSet(
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(
-        viewsets.GenericViewSet,
-        mixins.ListModelMixin,
-        mixins.CreateModelMixin):
+class TagViewSet(BaseRecipeAttrViewSet):
     '''
     Manage tags in the database
     '''
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
-    def get_queryset(self):
-        '''
-        Return objects for the current authenticated user only
-        '''
-        return self.queryset.filter(user=self.request.user).order_by('-name')
 
-    def perform_create(self, serializer):
-        '''
-        Create a new tag
-        '''
-        serializer.save(user=self.request.user)
-
-
-class IngredientViewSet(
-        viewsets.GenericViewSet, mixins.ListModelMixin,
-        mixins.CreateModelMixin):
+class IngredientViewSet(BaseRecipeAttrViewSet):
     '''
     Manage ingredients in the database
     '''
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-
-    def get_queryset(self):
-        '''
-        Return objects for the current authenticated user only
-        '''
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        '''
-        Create a new ingredient
-        '''
-        serializer.save(user=self.request.user)
