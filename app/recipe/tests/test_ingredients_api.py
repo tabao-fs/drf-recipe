9afbabab1df8590470b6edfed525b25285b76ef6
Tom <abaotom14@gmail.com>
2020-11-05 08:24:56 +0800

    add test_retrieve_ingredients_list

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -7,6 +7,7 @@ from rest_framework.test import APIClient
 
 from core.models import Ingredient
 
+from recipe.serializers import IngredientSerializer
 
 
 INGREDIENTS_URL = reverse('recipe:ingredient-list')
@@ -39,3 +40,17 @@ class PrivateIngredientsApiTests(TestCase):
         )
         self.client = APIClient()
         self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredients_list(self):
+        '''
+        Test retrieving a list of ingredients
+        '''
+        Ingredient.objects.create(user=self.user, name='Test 1')
+        Ingredient.objects.create(user=self.user, name='Test 2')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
