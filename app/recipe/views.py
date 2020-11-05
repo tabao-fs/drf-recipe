f335d1ddb3a1b0a60f522d3b0ce8cc1482680b60
Tom <abaotom14@gmail.com>
2020-11-05 08:33:10 +0800

    add get_queryset

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -39,3 +39,9 @@ class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+
+    def get_queryset(self):
+        '''
+        Return objects for the current authenticated user only
+        '''
+        return self.queryset.filter(user=self.request.user).order_by('-name')
