812395d4d9d68fa1c8669ce1e184146394903912
Tom <abaotom14@gmail.com>
2020-11-05 08:20:47 +0800

    add PublicIngredientsApiTests

---
+++ app/recipe/tests/test_ingredients_api.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Ingredient
+
+
+class PublicIngredientsApiTests(TestCase):
+    '''
+    Test the publicly available ingredients API
+    '''
+    def setUp(self):
+        self.client = APIClient()
