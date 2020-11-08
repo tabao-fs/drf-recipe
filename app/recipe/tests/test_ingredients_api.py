c56f5d5c4988d4b5c8e38d5e7ac94e4f1c6953b9
Tom <abaotom14@gmail.com>
2020-11-08 10:34:10 +0800

    add test_retrieve_ingredients_assigned_unique

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -100,7 +100,7 @@ class PrivateIngredientsApiTests(TestCase):
         Test filtering ingredients by those assigned to recipes
         '''
         ingredient1 = Ingredient.objects.create(user=self.user, name='Test 1')
-        ingredient1 = Ingredient.objects.create(user=self.user, name='Test 2')
+        ingredient2 = Ingredient.objects.create(user=self.user, name='Test 2')
         recipe = Recipe.objects.create(
             title='Recipe',
             time_minutes=5,
@@ -109,9 +109,34 @@ class PrivateIngredientsApiTests(TestCase):
         )
         recipe.ingredients.add(ingredient1)
 
-        self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
+        res = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
 
         serializer1 = IngredientSerializer(ingredient1)
         serializer2 = IngredientSerializer(ingredient2)
         self.assertIn(serializer1.data, res.data)
         self.assertNotIn(serializer2.data, res.data)
+
+    def test_retrieve_ingredients_assigned_unique(self):
+        '''
+        Test filtering ingredients by assigned returns unique items
+        '''
+        ingredient = Ingredient.objects.create(user=self.user, name='Test 1')
+        Ingredient.objects.create(user=self.user, name='Test 2')
+        recipe1 = Recipe.objects.create(
+            title='Recipe 1',
+            time_minutes=30,
+            price=12.00,
+            user=self.user
+        )
+        recipe1.ingredients.add(ingredient)
+        recipe2 = Recipe.objects.create(
+            title='Recipe 2',
+            time_minutes=20,
+            price=5.00,
+            user=self.user
+        )
+        recipe2.ingredients.add(ingredient)
+
+        res = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
+
+        self.assertEqual(len(res.data), 1)
