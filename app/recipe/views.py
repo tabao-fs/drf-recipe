eed45bb8bf4dba53b018cd56358c2d955a60853b
Tom <abaotom14@gmail.com>
2020-11-08 10:41:03 +0800

    add distinct

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -32,7 +32,9 @@ class BaseRecipeAttrViewSet(
         if assigned_only:
             queryset = queryset.filter(recipe__isnull=False)
 
-        return queryset.filter(user=self.request.user).order_by('-name')
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         '''
