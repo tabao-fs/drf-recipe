0dc0b626f5423cf9af3c12215f15c3140479e68f
Tom <abaotom14@gmail.com>
2020-11-08 08:33:13 +0800

    add recipe_image_file_path

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,21 @@
+import uuid
+import os
+
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, PermissionsMixin
 )
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    '''
+    Generate file path for new recipe image
+    '''
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/', filename)
 
 
 class UserManager(BaseUserManager):
